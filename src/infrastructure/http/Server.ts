4dceb186c85a57a9f74bec2860464d627d80aec5
daaxar <daaxar@gmail.com>
2024-02-10 19:32:07 -0300

    Funcionalidad basica de rutas

--- src/infrastructure/http/Server.ts
+++ src/infrastructure/http/Server.ts
@@ -1,24 +1,47 @@
 import { IncomingMessage, ServerResponse, createServer } from 'node:http';
 import { ConfigApplication } from '../../config/types';
 
+type RouteHandler = (
+    req: IncomingMessage,
+    res: ServerResponse<IncomingMessage>,
+) => void;
+type RouteDefinition = { path: RegExp | string; handler: RouteHandler };
+
 export function Server(config: ConfigApplication) {
+    const routes: RouteDefinition[] = [
+        {
+            path: '/',
+            handler: (req, res) => {
+                const now = new Date();
+                res.statusCode = 200;
+                res.setHeader('content-type', 'text/plain');
+
+                console.log(
+                    `${now.toISOString()}\t${config.hostname}\t${
+                        res.statusCode
+                    }\t${req.url}`,
+                );
+
+                res.write(
+                    `Hoy es ${new Date().toLocaleString()} en ${
+                        config.hostname
+                    } (${config.machine})`,
+                );
+            },
+        },
+    ];
+
     return createServer(
         (req: IncomingMessage, res: ServerResponse<IncomingMessage>): void => {
-            const now = new Date();
-            res.statusCode = req.url?.startsWith('/favicon.ico') ? 404 : 200;
-            res.setHeader('content-type', 'text/plain');
-
-            console.log(
-                `${now.toISOString()}\t${config.hostname}\t${res.statusCode}\t${
-                    req.url
-                }`,
+            const route = routes.find(({ path }) =>
+                path instanceof RegExp
+                    ? req.url?.match(path)
+                    : req.url === path,
             );
 
-            res.write(
-                `Hoy es ${new Date().toLocaleString()} en ${config.hostname} (${
-                    config.machine
-                })`,
-            );
+            if (!route) res.statusCode = 404;
+            else route.handler(req, res);
+
             res.end();
         },
     );
