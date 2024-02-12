2df8cbf2edb3188dc349a9cf02d83e1f736a5dc0
daaxar <daaxar@gmail.com>
2024-02-12 01:07:24 -0300

    La aplicación selecciona el idioma al azar

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,11 +1,16 @@
 import { ConfigApplication } from '../config/types';
 
+type Language = 'es' | 'en' | 'pt' | 'it' | 'fr' | 'de';
+
 export class AppService {
     constructor(private readonly config: ConfigApplication) {}
 
     async getMessage(): Promise<string> {
+        const lang = ['es', 'en', 'pt', 'it', 'fr', 'de'][
+            Math.floor(Math.random() * 6)
+        ];
         const [template, data] = await Promise.all([
-            this.getTemplate(),
+            this.getTemplate(lang),
             this.getData(),
         ]);
 
@@ -16,8 +21,21 @@ export class AppService {
         );
     }
 
-    private async getTemplate() {
-        return `Hoy es {{now}} en {{host}} ({{machine}})`;
+    private async getTemplate(lang: Language) {
+        const tempĺates = {
+            es: 'Hoy es {{now}} en {{host}} ({{machine}})',
+            en: 'Today is {{now}} in {{host}} ({{machine}})',
+            pt: 'Hoje é {{now}} em {{host}} ({{machine}})',
+            it: 'Oggi è {{now}} in {{host}} ({{machine}})',
+            fr: "Aujourd'hui, c'est {{now}} à {{host}} ({{machine}})",
+            de: 'Heute ist {{now}} in {{host}} ({{machine}})',
+        };
+
+        const template = tempĺates[lang];
+
+        if (!template) throw new Error('Idioma invalido');
+
+        return template;
     }
 
     private async getData() {
